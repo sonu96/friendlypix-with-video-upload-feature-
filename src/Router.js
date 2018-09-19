9ae528abf877880d39ccd0d4a7707b2a9494c08d
Abhinav Manyam <abhinavmanyam12@gmail.com>
2018-09-18 22:41:57 -0400

    Initial Commit

---
+++ src/Router.js
@@ -0,0 +1,154 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+import $ from 'jquery';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import {MaterialUtils} from './Utils';
+import page from 'page';
+
+/**
+ * Handles the pages/routing.
+ */
+export default class Router {
+  /**
+   * Initializes the Friendly Pix controller/router.
+   * @constructor
+   */
+  constructor(loadApp, auth) {
+    this.auth = auth;
+
+    // Dom elements.
+    this.pagesElements = $('[id^=page-]');
+    this.splashLogin = $('#login', '#page-splash');
+
+    // Make sure /add is never opened on website load.
+    if (window.location.pathname === '/add') {
+      page('/');
+    }
+
+    // Configuring routes.
+    const pipe = Router.pipe;
+    const displayPage = this.displayPage.bind(this);
+    const loadUser = (userId) => loadApp().then(({userPage}) => userPage.loadUser(userId));
+    const showHomeFeed = () => loadApp().then(({feed}) => feed.showHomeFeed());
+    const showGeneralFeed = () => loadApp().then(({feed}) => feed.showGeneralFeed());
+    const clearFeed = () => loadApp().then(({feed}) => feed.clear());
+    const showPost = (postId) => loadApp().then(({post}) => post.loadPost(postId));
+
+    page('/', pipe(showHomeFeed, null, true), pipe(displayPage, {pageId: 'feed', onlyAuthed: true}));
+    page('/feed', pipe(showGeneralFeed, null, true), pipe(displayPage, {pageId: 'feed'}));
+    page('/post/:postId', pipe(showPost, null, true), pipe(displayPage, {pageId: 'post'}));
+    page('/user/:userId', pipe(loadUser, null, true), pipe(displayPage, {pageId: 'user-info'}));
+    page('/about', pipe(clearFeed, null, true), pipe(displayPage, {pageId: 'about'}));
+    page('/terms', pipe(clearFeed, null, true), pipe(displayPage, {pageId: 'terms'}));
+    page('/add', pipe(displayPage, {pageId: 'add', onlyAuthed: true}));
+    page('*', () => page('/'));
+
+    // Start routing.
+    page();
+  }
+
+  /**
+   * Returns a function that displays the given page and hides the other ones.
+   * if `onlyAuthed` is set to true then the splash page will be displayed instead of the page if
+   * the user is not signed-in.
+   */
+  displayPage(attributes, context) {
+    const onlyAuthed = attributes.onlyAuthed;
+
+    if (onlyAuthed) {
+      // If the page can only be displayed if the user is authenticated then we wait or the auth state.
+      this.auth.waitForAuth.then(() => {
+        this._displayPage(attributes, context);
+      });
+    } else {
+      this._displayPage(attributes, context);
+    }
+  }
+
+  _displayPage(attributes, context) {
+    const onlyAuthed = attributes.onlyAuthed;
+    let pageId = attributes.pageId;
+
+    if (onlyAuthed && !firebase.auth().currentUser) {
+      pageId = 'splash';
+      this.splashLogin.show();
+    }
+    Router.setLinkAsActive(context.canonicalPath);
+    this.pagesElements.each(function(index, element) {
+      if (element.id === 'page-' + pageId) {
+        $(element).show();
+      } else if (element.id === 'page-splash' && onlyAuthed) {
+        $(element).fadeOut(1000);
+      } else {
+        $(element).hide();
+      }
+    });
+    MaterialUtils.closeDrawer();
+    Router.scrollToTop();
+  }
+
+  /**
+   * Reloads the current page.
+   */
+  static reloadPage() {
+    let path = window.location.pathname;
+    if (path === '') {
+      path = '/';
+    }
+    page(path);
+  }
+
+  /**
+   * Scrolls the page to top.
+   */
+  static scrollToTop() {
+    $('html,body').animate({scrollTop: 0}, 0);
+  }
+
+  /**
+   * Pipes the given function and passes the given attribute and Page.js context.
+   * Set 'optContinue' to true if there are further functions to call.
+   */
+  static pipe(funct, attribute, optContinue) {
+    return (context, next) => {
+      if (funct) {
+        const params = Object.keys(context.params);
+        if (!attribute && params.length > 0) {
+          funct(context.params[params[0]], context);
+        } else {
+          funct(attribute, context);
+        }
+      }
+      if (optContinue) {
+        next();
+      }
+    };
+  }
+
+  /**
+   * Highlights the correct menu item/link.
+   */
+  static setLinkAsActive(canonicalPath) {
+    if (canonicalPath === '') {
+      canonicalPath = '/';
+    }
+    $('.is-active').removeClass('is-active');
+    $(`[href="${canonicalPath}"]`).addClass('is-active');
+  }
+};
---
+++ src/Search.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+import $ from 'jquery';
+
+/**
+ * Handles the Friendly Pix search feature.
+ */
+export default class Search {
+  /**
+   * The minimum number of characters to trigger a search.
+   * @return {number}
+   */
+  static get MIN_CHARACTERS() {
+    return 3;
+  }
+
+  /**
+   * The maximum number of search results to be displayed.
+   * @return {number}
+   */
+  static get NB_RESULTS_LIMIT() {
+    return 10;
+  }
+
+  /**
+   * Initializes the Friendly Pix search bar.
+   */
+  constructor(firebaseHelper) {
+    this.firebaseHelper = firebaseHelper;
+
+    // DOM Elements pointers.
+    this.searchField = $('#searchQuery');
+    this.searchResults = $('#fp-searchResults');
+
+    // Event bindings.
+    this.searchField.keyup(() => this.displaySearchResults());
+    this.searchField.focus(() => this.displaySearchResults());
+    this.searchField.click(() => this.displaySearchResults());
+  }
+
+  /**
+   * Display search results.
+   */
+  displaySearchResults() {
+    const searchString = this.searchField.val().toLowerCase().trim();
+    if (searchString.length >= Search.MIN_CHARACTERS) {
+      this.firebaseHelper.searchUsers(searchString, Search.NB_RESULTS_LIMIT).then(
+          (results) => {
+            this.searchResults.empty();
+            const peopleIds = Object.keys(results);
+            if (peopleIds.length > 0) {
+              this.searchResults.fadeIn();
+              $('html').click(() => {
+                $('html').unbind('click');
+                this.searchResults.fadeOut();
+              });
+              peopleIds.forEach((peopleId) => {
+                const profile = results[peopleId];
+                this.searchResults.append(
+                    Search.createSearchResultHtml(peopleId, profile));
+              });
+            } else {
+              this.searchResults.fadeOut();
+            }
+          });
+    } else {
+      this.searchResults.empty();
+      this.searchResults.fadeOut();
+    }
+  }
+
+  /**
+   * Returns the HTML for a single search result
+   */
+  static createSearchResultHtml(peopleId, peopleProfile) {
+    return `
+        <a class="fp-searchResultItem fp-usernamelink mdl-button mdl-js-button" href="/user/${peopleId}">
+            <div class="fp-avatar"style="background-image: url(${peopleProfile.profile_picture ||
+                '/images/silhouette.jpg'})"></div>
+            <div class="fp-username mdl-color-text--black">${peopleProfile.full_name}</div>
+        </a>`;
+  }
+};
